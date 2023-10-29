aa98db416636a6d9b40b652d64b5af0db2f71ca7
felipecardona00 <felipecardona0304@gmail.com>
2023-10-29 15:55:47 -0500

    Hotel and Room Controllers added

---
+++ HotelNetwork_API_CardonaAndres/Controllers/HotelController.cs
@@ -0,0 +1,85 @@
+﻿using HotelNetwork_API_CardonaAndres.DAL.Entities;
+using HotelNetwork_API_CardonaAndres.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace HotelNetwork_API_CardonaAndres.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly IHotelService _hotelService;
+
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetAll")]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotelsAsync()
+        {
+            var hotels = await _hotelService.GetHotelsAsync();
+
+            if (hotels == null || !hotels.Any()) return NotFound();
+
+            return Ok(hotels);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetById/{id}")]
+        public async Task<ActionResult<Hotel>> GetHotelByIdAsync(Guid id)
+        {
+            if (id == default) return BadRequest("Id is required!");
+
+            var hotel = await _hotelService.GetHotelByIdAsync(id);
+
+            if (hotel == null) return NotFound();
+
+            return Ok(hotel);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetByCity/{city}")]
+        public async Task<ActionResult<Hotel>> GetHotelByCityAsync(string city)
+        {
+            if (city == null) return BadRequest("City name is required!");
+
+            var hotel = await _hotelService.GetHotelByCityAsync(city);
+
+            if (hotel == null) return NotFound();
+
+            return Ok(hotel);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit/{id}")]
+        public async Task<ActionResult<Hotel>> EditHotelAsync(Guid id, int stars)
+        {
+            try
+            {
+                var editedHotel = await _hotelService.EditHotelAsync(id,stars);
+                return Ok(editedHotel);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicate"))
+                    return Conflict(String.Format("Hotel with id {0} already exists",id));
+
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete")]
+        public async Task<ActionResult<Hotel>> DeleteHotelAsync(Guid id)
+        {
+            if (id == default) return BadRequest("Id is required!");
+
+            var deletedHotel = await _hotelService.DeleteHotelAsync(id);
+
+            if (deletedHotel == null) return NotFound("Hotel not found!");
+
+            return Ok(deletedHotel);
+        }
+    }
+}
---
+++ HotelNetwork_API_CardonaAndres/Controllers/RoomController.cs
@@ -0,0 +1,39 @@
+﻿using HotelNetwork_API_CardonaAndres.DAL.Entities;
+using HotelNetwork_API_CardonaAndres.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace HotelNetwork_API_CardonaAndres.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetByNumber/{number}")]
+        public async Task<ActionResult<Room>> GetRoomByNumberAsync(int number)
+        {
+            if (default(int) == number) return BadRequest("Id is required!");
+
+            var room = await _roomService.GetRoomByNumberAsync(number);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+            else if (!room.Availability)
+            {
+                return BadRequest($"Room {room.Number} of the hotel {room.Hotel?.Name} already booked");
+            }
+
+            return Ok(room);
+        }
+    }
+}
